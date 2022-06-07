c5bec6ab116e73a1c352cd34b11a40495d8d7000
lu-css <dossantossilvalucas963@gmail.com>
2022-06-06 23:47:36 -0300

    index

--- praga0/src/index.js
+++ praga0/src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { NavBar } from './NavBar/NavBar';
+import { NavBar } from './Components/NavBar/NavBar.js'
+import { Footer } from './Components/Footer/Footer.js'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
     <NavBar />
-  </React.StrictMode>
+    <App />
+    <Footer></Footer>
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
